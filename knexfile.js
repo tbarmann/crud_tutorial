b681ad1f4571fdae672ad0cc985392838dda3da8
Timothy Barmann <tbarmann92@gmail.com>
2017-07-21 08:56:26 -0400

    migrations

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'webstore',
+      user:     'webstore',
+      password: 'Password1'
+    }
+  }
+};
---
+++ stickers.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    url: 'http://devstickers.com/assets/img/cat/angular2.png',
+    rating: 5,
+    description: 'Angular 2 Logo',
+    title: 'Angular 2'
+  },
+  {
+    url: 'http://devstickers.com/assets/img/cat/ruby.png',
+    rating: 10,
+    description: 'Ruby Logo',
+    title: 'Ruby'
+  },
+  {
+    url: 'http://devstickers.com/assets/img/cat/handlebars-js.png',
+    rating: 6,
+    description: 'Handlebars Logo',
+    title: 'Handlebars'
+  },
+  {
+    url: 'http://devstickers.com/assets/img/cat/reddit.png',
+    rating: 7,
+    description: 'Reddit Logo',
+    title: 'Reddit'
+  }];
